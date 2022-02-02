44dce25ea392a60e58be470131444d7e69443787
Joshafutt <joshafuttcomp23@ucla.edu>
2022-02-02 12:45:44 -0800

    Test

--- text_publisher.py
+++ text_publisher.py
@@ -27,29 +27,34 @@ def on_message(client, userdata, message):
           message.topic + '" with QoS ' + str(message.qos))
 
 
-# 1. create a client instance.
-client = mqtt.Client()
-# add additional client options (security, certifications, etc.)
-# many default options should be good to start off.
-# add callbacks to client.
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
+def client_instance():
+    # 1. create a client instance.
+    client = mqtt.Client()
+    # add additional client options (security, certifications, etc.)
+    # many default options should be good to start off.
+    # add callbacks to client.
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
 
-# 2. connect to a broker using one of the connect*() functions.
-client.connect_async('test.mosquitto.org')
+    # 2. connect to a broker using one of the connect*() functions.
+    client.connect_async('test.mosquitto.org')
 
-# 3. call one of the loop*() functions to maintain network traffic flow with the broker.
-client.loop_start()
+    # 3. call one of the loop*() functions to maintain network traffic flow with the broker.
+    client.loop_start()
 
-# 4. use subscribe() to subscribe to a topic and receive messages.
+    # 4. use subscribe() to subscribe to a topic and receive messages.
 
-# 5. use publish() to publish messages to the broker.
-# payload must be a string, bytearray, int, float or None.
-text = 'My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'
+    # 5. use publish() to publish messages to the broker.
+    # payload must be a string, bytearray, int, float or None.
+    text = 'My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'
 
-client.publish('ece180d/text', text, qos=1)
-print("gets here")
-# 6. use disconnect() to disconnect from the broker.
-client.loop_stop()
-client.disconnect()
+    client.publish('ece180d/text', text, qos=1)
+    print("gets here")
+    # 6. use disconnect() to disconnect from the broker.
+    client.loop_stop()
+    client.disconnect()
+
+
+if __name__ == '__main__':
+    client_instance()
